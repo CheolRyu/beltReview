e7d1b40385426a35d95b20e646277ca230a72de9
Charles <charlesworld1994@gmail.com>
2018-05-23 18:01:12 -0700

    worked on basics

---
+++ apps/review/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import re
+import bcrypt
+from django.db import models
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9\.\+_-]+@[a-zA-Z0-9\._-]+\.[a-zA-Z]*$')
+NAME_REGEX = re.compile(r'^[A-Za-z]\w+$')
+
+class UserManager(models.Manager):
+    def validate_login(self, post_data):
+        errors = []
+# Create your models here.
